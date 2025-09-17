4fa5d29f4b6de4c679c42c10216f35ad5719c645
zlatakimjeon <zlatakimjeon@gmail.com>
2025-09-17 19:27:26 +0300

    1

---
+++ tests/subset_iterator.h
@@ -0,0 +1,20 @@
+// src/subset_iterator.h
+#ifndef SUBSET_ITERATOR_H
+#define SUBSET_ITERATOR_H
+
+#include "set.h"
+
+typedef struct {
+    int n;
+    int mask;
+    int total;
+    set_t value;
+} iterator_t;
+
+void iterator_init(iterator_t *it, int n);
+void iterator_destroy(iterator_t *it);
+int iterator_has_next(const iterator_t *it);
+void iterator_next(iterator_t *it);
+const set_t *iterator_value(const iterator_t *it);
+
+#endif
